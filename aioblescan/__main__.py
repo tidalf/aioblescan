c678095a101008444109cb6a1a42f1af21fbf169
tidalf <tidalf@ematome.com>
2018-07-26 13:33:46 +0000

    fixpy3.6

--- aioblescan/__main__.py
+++ aioblescan/__main__.py
@@ -104,7 +104,13 @@ event_loop = asyncio.get_event_loop()
 mysocket = aiobs.create_bt_socket(mydev)
 
 #create a connection with the raw socket
-fac=event_loop.create_connection(aiobs.BLEScanRequester,sock=mysocket)
+fac=event_loop._create_connection_transport(
+        mysocket,
+        aiobs.BLEScanRequester,
+        None,
+        None)
+
+# fac=event_loop.create_connection(aiobs.BLEScanRequester,sock=mysocket)
 #Start it
 conn,btctrl = event_loop.run_until_complete(fac)
 #Attach your processing
